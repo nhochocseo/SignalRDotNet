2b32ea5f0238b5a4a839313827f3a7c2f7948f32
thienth3 <thienth3@gmail.com>
2018-10-10 10:34:34 +0700

    new project

---
+++ wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// creater a connection for signal r
+const connection = new signalR.HubConnectionBuilder()
+.withUrl("/chatHub")
+.build();
+
+// recevice the message
+connection.on("ReceiveMessage",(user,message) => {
+    const rec_msg = user + ":" + message;
+    const li = document.createElement("li");
+    li.textContent = rec_msg;
+    document.getElementById("messagesList").appendChild(li);
+})
+
+connection.start().catch(err => console.err(err.toString()));
+
+//send the message
+document.getElementById("sendMessage").addEventListener("click",event => {
+    const user = document.getElementById("userName").value;
+    const message = document.getElementById("userMessage").value;
+
+    connection.invoke("SendMessage",user,message).catch(err => console.error(err.toString()));
+    event.preventDefault();
+});
+
